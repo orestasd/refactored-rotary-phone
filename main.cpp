034434425fd7540acafedba08ac098bb67aaa5e0
orestasd <orestinas9@gmail.com>
2019-05-25 14:25:30 +0300

    Pradzia

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const char LANG_LAIVAS = '*';
+const char LANG_SAUTA = ' ';
+const char LANG_PATAIK = 'X';
+
+class Laivai{
+    private:
+
+    public:
+
+};
+class ZaidimoLenta {
+    private:
+            char zaidLaukas[10][10];
+            char priesLaukas[10][10];
+    public:
+
+};
+
+int  main()
+{
+  
+
+    return 0;
+}
